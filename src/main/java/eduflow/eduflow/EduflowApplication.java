d2ce204002615bb7a0b11282a8c378ee09870b56
codingrev <emmowu10@gmail.com>
2022-07-06 21:40:09 +0000

    feat: project startup

--- src/main/java/eduflow/eduflow/EduflowApplication.java
+++ src/main/java/eduflow/eduflow/EduflowApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EduflowApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EduflowApplication.class, args);
 	}
 
